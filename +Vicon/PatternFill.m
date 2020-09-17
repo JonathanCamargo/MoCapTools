f0e3a26fd374627a801d85eae05e2b89de86ee32
Jonathan <jcl00087@gmail.com>
2020-09-17 12:23:42 -0400

    Updated filling functions to take tables

--- +Vicon/PatternFill.m
+++ +Vicon/PatternFill.m
@@ -1,4 +1,4 @@
-function markerData = PatternFill(markerData, markerToFill, donor, t0, t1)
+function markersData = PatternFill(markersData, markerToFill, donor, t0, t1,varargin)
 % newMarkerData = PatternFill(markerData, markerToFill, donor, t0, t1)
 %
 % PatternFill fills a gap between frames t0 and t1 in a marker whose name
@@ -12,23 +12,42 @@ function markerData = PatternFill(markerData, markerToFill, donor, t0, t1)
 % Ex. For a marker 'BadMarker' with a gap between frames 500-700 (where all
 % of these frames are missing data for 'BadMarker'), to be filled using the
 % marker 'GoodMarker', given data in markerData: 
-% newMarkerData = Vicon.PatternFill(markerData, 'BadMarker', 'GoodMarker', 499, 701)
+% newMarkerData = PatternFill(markerData, 'BadMarker', 'GoodMarker', 499, 701)
+%
+% Options (defaults):
+% 'FW' true/(false) Use fw algorithm to reduce constraints (less accuracy but less restrictions)
 
-    assert(isstruct(markerData), 'GapFill:BadInput', 'Marker data must be provided as a struct of trajectories.')
+p=inputParser();
+p.addParameter('FW',false,@islogical);
+p.parse(varargin{:});
+
+FW=p.Results.FW;
+
+    assert(isstruct(markersData), 'GapFill:BadInput', 'Marker data must be provided as a struct of trajectories.')
     % identify best donor based on which is closest
+    markerData=markersData.(markerToFill); 
+    header=markerData.Header;
+    t0_idx=find(header==t0,1);
+    t1_idx=find(header==t1,1);
     if iscell(donor)
-        % remove invalid donors with no data in the gap
-        goodDonorMask = ~cellfun(@(d) all(all(isnan(markerData.(d)(t0+1:t1-1, :)))), donor);
+        % remove invalid donors with no data in the gap        
+        goodDonorMask = ~cellfun(@(d) all(all(isnan(markersData.(d){t0_idx+1:t1_idx-1, 2:end}))), donor);
         donor = donor(goodDonorMask);
         if isempty(donor)
             error('GapFill:NoDonors', 'No valid donors.')
         end
-        startDists = cellfun(@(d) norm(markerData.(d)(t0, :) - markerData.(markerToFill)(t0, :)), donor);
-        endDists = cellfun(@(d) norm(markerData.(d)(t1, :) - markerData.(markerToFill)(t1, :)), donor);
+        startDists = cellfun(@(d) norm(markersData.(d){t0_idx, 2:end} - markersData.(markerToFill){t0_idx, 2:end}), donor);
+        endDists = cellfun(@(d) norm(markersData.(d){t1_idx, 2:end} - markersData.(markerToFill){t1_idx, 2:end}), donor);
         dists = (startDists + endDists)/2;
         [~, bestDonorIdx] = min(dists);
         donor = donor{bestDonorIdx};
     end
-    traj = patternFill(markerData.(markerToFill), markerData.(donor), t0, t1);
-    markerData.(markerToFill) = traj;
+	if FW
+		traj = patternFillFW(markersData.(markerToFill){:,2:end}, markersData.(donor){:,2:end}, t0_idx, t1_idx);
+	else
+		traj = patternFill(markersData.(markerToFill){:,2:end}, markersData.(donor){:,2:end}, t0_idx, t1_idx);
+	end
+    z=markersData.(markerToFill);
+    z{t0_idx:t1_idx,2:end}=traj(t0_idx:t1_idx,:);
+    markersData.(markerToFill)=z;        
 end
