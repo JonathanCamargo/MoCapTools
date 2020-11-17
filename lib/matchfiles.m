767331646d92437805e5e02d7190fc249bd00bb9
Jonathan Camargo Leyva <jon-cama@gatech.edu>
2020-11-16 23:17:08 -0500

    matchfiles is lowercase

--- lib/matchfiles.m
+++ lib/matchfiles.m
@@ -1,16 +1,16 @@
 function files = matchfiles(varargin)
-% files = matchFiles(dirArg)
-% files = matchFiles(filepart1, ..., filepartN)
+% files = matchfiles(dirArg)
+% files = matchfiles(filepart1, ..., filepartN)
 % Returns a cell array containing the full path of all files and folders
 % that would be matched by dir(dirArg). Uses the same syntax as dir(),
 % including using '**' to recurse through subdirectories. If multiple
 % inputs are given, they will be combined with fullfile() before being
 % passed to dir.
 % Example usages: 
-% files = matchFiles('C:/');
-% files = matchFiles('./*.txt');
-% files = matchFiles(folder, '*.txt');
-% files = matchFiles(folder, '**/*.txt');
+% files = matchfiles('C:/');
+% files = matchfiles('./*.txt');
+% files = matchfiles(folder, '*.txt');
+% files = matchfiles(folder, '**/*.txt');
 
     files = dir(fullfile(varargin{:}));
     files = arrayfun(@(x) {fullfile(x.folder, x.name)}, files);
